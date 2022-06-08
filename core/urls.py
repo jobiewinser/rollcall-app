63ad10e08abd75c03a537929b5ff62a4730f2a9b
jobie winser <jobiewinser@live.co.uk>
2022-06-08 22:00:12 +0100

    rejigged, basic structure started

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rollcall', views.RollcallView.as_view(), name='rollcall'),
-    path('', views.SplashView.as_view(), name='splash'),
+    # path('rollcall', views.RollcallView.as_view(), name='rollcall'),
+    path('', views.LoginView.as_view()),
+    # path('login', views.LoginView.as_view(), name='login'),
+    path('logout', views.logout_htmx),
+    path('rollcall-form', views.rollcall_form),
+    path('set-up-rollcall', views.set_up_rollcall_form),
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import View, TemplateView
 from django.contrib.auth import authenticate, logout, login
@@ -14,13 +14,49 @@ from .forms import *
 from django.core.mail import EmailMessage
 from django.db.models import Q
 
-class LoginView(SuccessMessageMixin, LoginView):
-    template_name = 'login.html'
-    success_url = "/management"
+# class LoginView(SuccessMessageMixin, LoginView):
+#     template_name = 'login.html'
+#     success_url = "/management"
+#     form_class = UserLoginForm
+#     success_message = "Successful Login"
+
+# class RollcallView(TemplateView):
+#     template_name = "rollcall.html"
+
+# class SplashView(TemplateView):
+#     template_name = "splash.html"
+
+
+# def index(request):
+    # if request.user.is_authenticated:
+    # return render(request, 'index.html')
+    # return redirect('login')
+
+def logout_htmx(request):
+    logout(request)
+    return render(request, 'index.html')
+
+def rollcall_form(request):    
+    return render(request, 'rollcall_form.html')
+
+def set_up_rollcall_form(request):    
+    if request.META.get("HTTP_HX_REQUEST") != 'true':
+        # return redirect('index')
+        return render(request, 'set_up_rollcall_form_full.html', {})
+
+    return render(request, 'set_up_rollcall_form.html')
+
+class LoginView( LoginView):
+    template_name = 'index.html'
+    success_url = "/"
     form_class = UserLoginForm
     success_message = "Successful Login"
 
-class RollcallView(TemplateView):
-    template_name = "rollcall.html"
-class SplashView(TemplateView):
-    template_name = "splash.html"
+class LogoutView(View):
+    def get(self, request):
+        return HttpResponseRedirect("/")
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect("/")
